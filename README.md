bf348c18b3c2b283b2986776102a5636313f0224
Leonardo Bonetti <leonardo.bonetti@outlook.com>
2022-09-28 12:50:42 -0300

    chore: initialize

---
+++ README.md
@@ -0,0 +1,1 @@
+# Descentralized Agreements Network SDK
---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**',
+    '!<rootDir>/src/infra/db/typeorm/migrations/*.ts'
+  ],
+
+  moduleDirectories: ['node_modules', 'src'],
+
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  transform: { '.+\\.ts$': 'ts-jest' },
+  testMatch: ['**/*.spec.ts'],
+
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
